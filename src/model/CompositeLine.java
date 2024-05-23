2de7c4f10588429d22682c798b48aac2f8e89b42
thetruebrew <oscar.hermann94@gmail.com>
2024-05-23 09:13:23 +0200

    CompositeLine

---
+++ src/model/CompositeLine.java
@@ -0,0 +1,13 @@
+package model;
+import model.CompsiteProduct;
+import model.Product;
+
+public class CompositeLine {
+	private int quantity;
+	private Product product;
+	
+	public CompositeLine() {
+		this.quantity = quantity;
+		this.product = product; 
+	}
+}
