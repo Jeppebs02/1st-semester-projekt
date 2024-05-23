944efb814483340f5e485acf7933f27a3ff5cb60
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2024-05-23 09:13:17 +0200

    Create CompositeProduct.java
    Added composite product class

---
+++ src/model/CompositeProduct.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.ArrayList;
+import model.CompositeLine;
+
+public class CompositeProduct extends Product {
+
+	private String assemblyInstructions;
+	private ArrayList<CompositeLine> components;
+	
+
+	public CompositeProduct(String name, String barcode, String description, String sKU, String category,
+			ItemPrice price, Location iventory, String assemblyInstructions, ArrayList<CompositeLine> components) {
+		super(name, barcode, description, sKU, category, price, iventory);
+		this.assemblyInstructions = assemblyInstructions;
+		this.components = components;
+	}
+	
+	public String getBarcode() {
+		return super.getBarcode();
+	}
+	
+	
+
+}
