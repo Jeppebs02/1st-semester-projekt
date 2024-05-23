124fbef69c8ff408f886e67a4fefedfeb19a3f3d
jimil1 <46991972+jimil1@users.noreply.github.com>
2024-05-23 09:25:53 +0200

    specefic og copy

---
+++ src/model/Copy.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Copy {
+	private String serialNr;
+
+	public Copy(String serialNr) {
+		this.serialNr = serialNr;
+	}
+	
+	public String getSerialNr() {
+		return serialNr;
+		
+	}
+	
+}
