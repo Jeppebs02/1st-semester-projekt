0b95d6ea64417db79c07bf7588562c87731bb11a
jimil1 <46991972+jimil1@users.noreply.github.com>
2024-05-27 09:53:01 +0200

    junit test1

--- src/JUnitTest/ControllerTest.java
+++ src/JUnitTest/ControllerTest.java
@@ -1,34 +1,55 @@
 package JUnitTest;
+import model.Product;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.api.AfterAll;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-class ControllerTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+import org.junit.*;
+public class ControllerTest {
+	
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void Test1() {
+		Product test1 = new Product("Jeppe", null, null, null, null, null, null);
+		String output_f = test1.getName();
+		
+		assertEquals("Jeppe",output_f);
 	}
+	
+	public void Test2() {
+		
+		
+	}
+	
 
+//	\*
+//	@BeforeAll
+//	static void setUpBeforeClass() throws Exception {
+//	}
+//
+//	@AfterAll
+//	static void tearDownAfterClass() throws Exception {
+//	}
+//
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+//	@AfterEach
+//	void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+//	*\
 }
