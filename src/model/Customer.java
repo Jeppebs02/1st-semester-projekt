ee8be61109a1a1dfa1589b3434649d6a4d9b081c
thetruebrew <oscar.hermann94@gmail.com>
2024-05-27 09:19:01 +0200

    JUnit-test class
    +exceptions

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -11,17 +11,23 @@ public class Customer extends Person{
 	}
 
 
-	public String getCustomerID() {
+	public String getCustomerID() throws NullPointerException {
+		if (customerID == null) {
+            throw new NullPointerException("Customer ID er Null");
+        }
 		return customerID;
 	}
 
 
-	public CustomerCategory getCustomerCategory() {
+	public CustomerCategory getCustomerCategory() throws NullPointerException {
+		if (customerCategory == null) {
+            throw new NullPointerException("Customer ID er Null");
+        }
 		return customerCategory;
 	}
 
 
-	public void setCustomerCategory(CustomerCategory customerCategory) {
+	public void setCustomerCategory(CustomerCategory customerCategory) throws IllegalArgumentException {
 		this.customerCategory = customerCategory;
 	}
 	
