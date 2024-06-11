a8d91987c5238f8743ca4e701cced2926d2a8661
thetruebrew <oscar.hermann94@gmail.com>
2024-06-11 12:36:06 +0200

    Refactor + weight login GUI

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -19,13 +19,28 @@ import javax.swing.SwingConstants;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JPasswordField passwordField;
 	private JTextField textField;
+	private JPasswordField passwordField;
+	private JPanel southPanel;
+	private JButton btnLogin;
+	private JLabel lblPassword;
+	private JLabel lblUsername;
+	private JPanel centerPanel;
+	private JLabel lblLoginHeader;
+	private JPanel northPanel;
+	private GridBagLayout gbl_centerPanel;
+	private GridBagConstraints gbc_lblUsername;
+	private GridBagConstraints gbc_usernameField;
+	private GridBagConstraints gbc_lblPassword;
+	private GridBagConstraints gbc_passwordField;
 
 	/**
 	 * Launch the application.
@@ -48,84 +63,80 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(450, 300, 450, 300);
+		setBounds(450, 300, 562, 300);
 		contentPane = new JPanel();
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 3, 0, 0));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JLabel label = new JLabel("");
-		contentPane.add(label);
+		northPanel = new JPanel();
+		contentPane.add(northPanel, BorderLayout.NORTH);
 		
+		lblLoginHeader = new JLabel("Login på systemet");
+		lblLoginHeader.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		northPanel.add(lblLoginHeader);
 		
+		centerPanel = new JPanel();
+		contentPane.add(centerPanel, BorderLayout.CENTER);
+		gbl_centerPanel = new GridBagLayout();
+		gbl_centerPanel.columnWidths = new int[]{218, 218, 0};
+		gbl_centerPanel.rowHeights = new int[]{65, 22, 65, 0};
+		gbl_centerPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_centerPanel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		centerPanel.setLayout(gbl_centerPanel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Login på systemet");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel label_1 = new JLabel("");
-		contentPane.add(label_1);
-		
-		JLabel lblUsername = new JLabel("Employe ID");
-		contentPane.add(lblUsername);
-		
-		JLabel label_2 = new JLabel("");
-		contentPane.add(label_2);
+		lblUsername = new JLabel("Employe ID");
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		gbc_lblUsername = new GridBagConstraints();
+		gbc_lblUsername.fill = GridBagConstraints.VERTICAL;
+		gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsername.gridx = 0;
+		gbc_lblUsername.gridy = 0;
+		centerPanel.add(lblUsername, gbc_lblUsername);
 		
 		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		textField.setColumns(10);
-		contentPane.add(textField);
+		gbc_usernameField = new GridBagConstraints();
+		gbc_usernameField.fill = GridBagConstraints.BOTH;
+		gbc_usernameField.insets = new Insets(0, 0, 5, 0);
+		gbc_usernameField.gridx = 1;
+		gbc_usernameField.gridy = 0;
+		gbc_usernameField.weightx = 1.0;
+		centerPanel.add(textField, gbc_usernameField);
 		
-		JLabel label_3 = new JLabel("");
-		contentPane.add(label_3);
-		
-		JLabel label_4 = new JLabel("");
-		contentPane.add(label_4);
-		
-		JLabel label_5 = new JLabel("");
-		contentPane.add(label_5);
-		
-		JLabel lblNewLabel = new JLabel("Password");
-		contentPane.add(lblNewLabel);
-		
-		JLabel label_6 = new JLabel("");
-		contentPane.add(label_6);
+		lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.fill = GridBagConstraints.VERTICAL;
+		gbc_lblPassword.insets = new Insets(0, 0, 0, 5);
+		gbc_lblPassword.gridx = 0;
+		gbc_lblPassword.gridy = 2;
+		centerPanel.add(lblPassword, gbc_lblPassword);
 		
 		passwordField = new JPasswordField();
-		contentPane.add(passwordField);
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-                logIn();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.fill = GridBagConstraints.BOTH;
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 2;
+		centerPanel.add(passwordField, gbc_passwordField);
+		
+		southPanel = new JPanel();
+		contentPane.add(southPanel, BorderLayout.SOUTH);
+		
+		btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				handleLoginButton();
 			}
 		});
-		
-		JLabel label_7 = new JLabel("");
-		contentPane.add(label_7);
-		
-		JLabel label_8 = new JLabel("");
-		contentPane.add(label_8);
-		
-		JLabel label_9 = new JLabel("");
-		contentPane.add(label_9);
-		
-		JLabel label_10 = new JLabel("");
-		contentPane.add(label_10);
-		
-		JLabel label_11 = new JLabel("");
-		contentPane.add(label_11);
-		contentPane.add(btnNewButton);
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		southPanel.add(btnLogin);
 	}
 
-	protected void logIn() {
-		//Check om username og password er i system 
-		Menu menu = new Menu();
-		menu.setVisible(true);
-        Login.this.setVisible(false);
-        Login.this.dispose();
+	private void handleLoginButton() {
+		// TODO Write Login method. Use LoginController to check for employee login. 
 		
 	}
 	
